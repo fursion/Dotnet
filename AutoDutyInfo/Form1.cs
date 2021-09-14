1a7df6db4f00ceee4c23748ea31ad90d5d6b0d2c
fursion <fursion@fursion.cn>
2021-09-14 19:43:12 +0800

    update

--- AutoDutyInfo/Form1.cs
+++ AutoDutyInfo/Form1.cs
@@ -12,11 +12,33 @@ namespace AutoDutyInfo
 {
     public partial class Form1 : Form
     {
+        public void CreateControl()
+        {
+
+        }
+        public Label label1 = new Label()
+        {
+            Text = "AutoCreateString",
+            Location = new Point(10, 10),
+            TabIndex = 10
+        };
+        public TextBox textBox = new TextBox()
+        {
+            Location = new Point(10,10+20)
+        };
         public Form1()
         {
+            Controls.Add(label1);
+            Controls.Add(textBox);
             InitializeComponent();
+            FormLayout();
             System.Console.WriteLine("test");
         }
-
+        public void FormLayout()
+        {
+            this.Name = "Auto";
+            this.Size = new System.Drawing.Size(900, 500);
+            this.StartPosition = FormStartPosition.WindowsDefaultLocation;
+        }
     }
 }
