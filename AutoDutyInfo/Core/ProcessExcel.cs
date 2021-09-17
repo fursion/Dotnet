809dc8e3d8c7cb890dded8f4570cbf5372c1682a
fursion <fursion@fursion.cn>
2021-09-17 14:56:53 +0800

    Update

---
+++ AutoDutyInfo/Core/ProcessExcel.cs
@@ -0,0 +1,15 @@
+using System;
+using System.IO;
+using System.Data.OleDb;
+using NPOI;
+using NPOI.SS.UserModel;
+
+namespace  AutoDutyInfo.Core
+{
+    public class ExcelTools
+    {
+        public void ReadExcel(){
+            
+        }
+    }
+}
