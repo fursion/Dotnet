97245286b73936e16f55f629e5cf55a9179036f8
洁 杜 <604357968@qq.com>
2021-09-19 23:15:11 +0800

    update

--- AutoDutyInfo/Core/ExcelDownload.cs
+++ AutoDutyInfo/Core/ExcelDownload.cs
@@ -40,6 +40,7 @@ namespace AutoDutyInfo.Core
             }
             
         }
+        
 
     }
 }
--- AutoDutyInfo/Form1.cs
+++ AutoDutyInfo/Form1.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Net.Http;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using AutoDutyInfo.Core;
@@ -36,14 +38,60 @@ namespace AutoDutyInfo
             }
             textBox_result.NewLine(DutyInfo.Templatedict["班次表尾"]);
         }
-        private void button_update_Click(object sender, EventArgs e)
+        private async void button_update_Click(object sender, EventArgs e)
         {
-            Form2 form = new Form2();
-            form.test();
-            form.Show();
-            ExcelDownload.Test();
+           var str=await GetAccess_Token();
+           textBox_result.NewLine(str);
+           System.Console.WriteLine(str);
         }
-
+        public async Task<string> GetTask()
+        {
+            string feishufileUrl = "https://open.feishu.cn/open-apis/drive/v1/files/:{0}/download";
+            var fileurl = string.Format(feishufileUrl, "MOF7ydyKaiUgJ7QDMhsMb");
+            using (HttpClient request = new HttpClient())
+            {
+                try
+                {
+                    request.DefaultRequestHeaders.Add("Authorization", "Bearer t-87acec7f7f5f5e258ba9ef754bdcd8fb379372ef");
+                    System.Console.WriteLine(request.DefaultRequestHeaders);
+                    HttpResponseMessage response = await request.GetAsync(fileurl);
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    return responseBody;
+                }
+                catch (HttpRequestException ex)
+                {
+                    System.Console.WriteLine(ex.Message);
+                    throw ex;
+                }
+            }
+        }
+        public async Task<string> GetAccess_Token()
+        {
+            using (HttpClient request = new HttpClient())
+            {
+                try
+                {
+                    var body = new
+                    {
+                        app_id = "cli_a1ca8d842878d00c",
+                        app_secret = "RNT9QYI0FHGhpZQEvLhtmepxUD401NtT"
+                    };
+                    var Json = JsonSerializer.Serialize(body);
+                    using (var stringcontent = new StringContent(Json, Encoding.UTF8, "application/json"))
+                    {
+                        var response=await request.PostAsync("https://open.feishu.cn/open-apis/auth/v3/app_access_token/internal",stringcontent);
+                        var str=await response.Content.ReadAsStringAsync();
+                        request.Dispose();
+                        return str;
+                    }
+                }
+                catch (HttpRequestException ex)
+                {
+                    System.Console.WriteLine(ex.Message);
+                    throw ex;
+                }
+            }
+        }
         private void pictureBox1_Click(object sender, EventArgs e)
         {
 
