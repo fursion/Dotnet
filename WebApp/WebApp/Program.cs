d7249b378eee0a144d66671c7ca949898577a04d
洁 杜 <604357968@qq.com>
2021-09-20 23:07:56 +0800

    update

--- WebApp/WebApp/Program.cs
+++ WebApp/WebApp/Program.cs
@@ -1,15 +1,22 @@
-﻿using System;
-using Microsoft.AspNetCore.Http; 
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-namespace WebServer
+using Microsoft.Extensions.Logging;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
---
+++ WebServer/WebAPI/APITest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebServer.WebAPI
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class APITest : ControllerBase
+    {
+        [HttpGet]
+        public string Test()
+        {
+            return "test success";
+        }
+        [HttpGet("urltest")]
+        public string Get()
+        {
+            return "urltest";
+        }
+        [HttpPost("post")]
+        public string Info()
+        {
+            return "Post info";
+        }
+    }
+}
