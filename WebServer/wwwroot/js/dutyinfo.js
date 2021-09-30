1f0bc5f6f6e0f82264e42e8d147bfd8ba8584a1f
fursion <fursion@fursion.cn>
2021-09-30 16:18:54 +0800

    update

---
+++ WebServer/wwwroot/js/dutyinfo.js
@@ -0,0 +1,10 @@
+
+function copytext() {
+    var textarea = document.getElementById('duty');
+    var text = textarea.value;
+    textarea.select();
+    document.execCommand('copy');
+}
+function copy(assname){
+
+}
