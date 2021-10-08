ab57876d7a33eca7db5bb6079fcded41d92c786a
fursion <fursion@fursion.cn>
2021-10-08 10:36:28 +0800

    update

---
+++ QRCode/QRCode/CreateQRCode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using QRCoder;
+using System.Drawing;
+namespace QRCode
+{
+    public static class CreateQRCode
+    {
+        public static void Create(string text)
+        {
+            QRCodeGenerator qRCodeGenerator = new();
+            QRCodeData qRCodeData = qRCodeGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
+            QRCoder.QRCode qRCode = new QRCoder.QRCode(qRCodeData);
+            Bitmap qrbitmap = qRCode.GetGraphic(20);
+        }
+    }
+}
